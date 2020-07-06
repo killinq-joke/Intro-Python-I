0bb240b4a705d89a379d226e12356ce08235ebef
zakaria touzri <zakadmaster@gmail.com>
2020-07-06 21:41:00 +0200

    ADDED: f1 & f2é

--- src/11_args.py
+++ src/11_args.py
@@ -5,6 +5,8 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
+def f1(n1, n2):
+    return n1 + n2
 
 print(f1(1, 2))
 
@@ -13,7 +15,9 @@ print(f1(1, 2))
 # Note: Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
-
+def f2(*args):
+    return sum(args)
+    
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
 print(f2(1, 4, -12))            # Should print -7
