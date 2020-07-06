68799d81955516f1df0dfbc135428a458e761f74
zakaria touzri <zakadmaster@gmail.com>
2020-07-06 22:17:04 +0200

    FIXED f2

--- src/11_args.py
+++ src/11_args.py
@@ -16,8 +16,11 @@ print(f1(1, 2))
 
 # YOUR CODE HERE
 def f2(*args):
-    return sum(args)
-    
+    if isinstance(args[0], list):
+        return sum(args[0])
+    else:
+        return sum(args)
+
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
 print(f2(1, 4, -12))            # Should print -7
@@ -34,6 +37,8 @@ print(f2(a))    # Should print 22
 # Note: Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
+def f3(a, b=None):
+    return a+b if b is not None else a + 1
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
