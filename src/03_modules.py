29577ff030b663eda8a096c861a4f956663ebbef
zakaria touzri <zakadmaster@gmail.com>
2020-07-06 19:09:30 +0200

    ADDED: sys answers

--- src/03_modules.py
+++ src/03_modules.py
@@ -15,10 +15,11 @@ for i in sys.argv:
 
 # Print out the OS platform you're using:
 # YOUR CODE HERE
-
+print(sys.platform)
 
 # Print out the version of Python you're using:
 # YOUR CODE HERE
+print(sys.version)
 
 
 import os
